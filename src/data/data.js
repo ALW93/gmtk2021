9820119dbb19a2768242708e4002cc129deb31cd
Alycia Wang <AlyciaLWang@gmail.com>
2021-06-11 19:23:30 -0400

    Centralize data imports

---
+++ src/data/data.js
@@ -0,0 +1,13 @@
+const ingredients = require("./ingredients");
+const ailments = require("./ailments");
+const npc = require("./npc");
+const potions = require("./potions");
+
+const data = {
+  ingredients,
+  ailments,
+  npc,
+  potions,
+};
+
+module.exports = data;

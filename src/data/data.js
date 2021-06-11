413c8a027b6650d1a9c4b9d3bc1559f35778c87a
Alycia Wang <AlyciaLWang@gmail.com>
2021-06-11 19:59:49 -0400

    Fix typo

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 const ingredients = require("./ingredients");
 const ailments = require("./ailments");
-const npc = require("./npc");
+const npc = require("./npcs");
 const potions = require("./potions");
 
 const data = {

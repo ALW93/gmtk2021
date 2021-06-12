cf2138fd22d3df2d5afee8edd57582530a59123b
Michael Tan <tan00060@algonquinlive.com>
2021-06-12 01:12:53 -0400

    added img/alt

--- src/components/Discovery.js
+++ src/components/Discovery.js
@@ -4,7 +4,11 @@ import { useSelector } from "react-redux";
 const Discovery = () => {
   const result = useSelector((state) => state.result);
 
-  return <div className="discoveryContainer">{JSON.stringify(result)}</div>;
+  return (
+    <div className="discoveryContainer">
+      <img src={result.url} alt={result.name}></img>
+    </div>
+  );
 };
 
 export default Discovery;

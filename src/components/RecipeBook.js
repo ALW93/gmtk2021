fc6a1836df82012d58897dc0522b8ca0e9036488
Alycia Wang <AlyciaLWang@gmail.com>
2021-06-12 18:56:22 -0400

    Set up layout for recipe book

--- src/components/RecipeBook.js
+++ src/components/RecipeBook.js
@@ -6,25 +6,26 @@ import Item from "../components/shared/Item";
 
 const RecipeBook = ({ title, onClick }) => {
   const unlocked = Object.keys(loadSaveData());
-  console.log(unlocked);
 
   return (
     <LightBox onClick={onClick}>
-      <h1>{title}</h1>
       <div className="recipeContainer">
-        {potions.map((potion) => {
-          const discovered = unlocked.includes(potion.id);
-          console.log(discovered);
-          return (
-            <Item
-              key={potion.id}
-              id={potion.id}
-              type="potion"
-              name={potion.name}
-              disabled={!discovered}
-            />
-          );
-        })}
+        <div className="recipeContainer--list">
+          <h1>{title}</h1>
+          {potions.map((potion) => {
+            const discovered = unlocked.includes(potion.id);
+            return (
+              <Item
+                key={potion.id}
+                id={potion.id}
+                type="potion"
+                name={potion.name}
+                disabled={!discovered}
+              />
+            );
+          })}
+        </div>
+        <div className="recipeContainer--preview">potion preview</div>
       </div>
     </LightBox>
   );

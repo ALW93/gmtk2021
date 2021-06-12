b7e3527df8ed4da23bac1e38361351dbd9d3d863
Alycia Wang <AlyciaLWang@gmail.com>
2021-06-11 23:41:09 -0400

    Create discovery panel

---
+++ src/components/Discovery.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Discovery = (props) => {
+  return <div className="discoveryContainer">Outcome</div>;
+};
+
+export default Discovery;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,20 +1,21 @@
 import React from "react";
 import Workbench from "./Workbench";
 import NPC from "./NPC";
+import Discovery from "./Discovery";
 
 const Game = (props) => {
   return (
     <div className="GameContainer grid">
-        <aside>
-          <Workbench />
-        </aside>
-        <main className="main">
-          <h1 class="title">Potion Matching Game</h1>
-          <NPC />
-        </main>
-        <aside>
-          <p>Right column</p>
-        </aside>
+      <aside>
+        <Discovery />
+      </aside>
+      <main className="main">
+        <h1 class="title">Potion Matching Game</h1>
+        <NPC />
+      </main>
+      <aside>
+        <Workbench />
+      </aside>
     </div>
   );
 };

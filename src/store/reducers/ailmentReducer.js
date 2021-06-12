3de02a096be502ee37fc1d6cdd6bd9d104b87508
Yuka Moribe <60080626+smilelk4@users.noreply.github.com>
2021-06-12 11:06:29 -0400

    Implement functionality to display success/failure dialogue upon potion is concocted (#17)

---
+++ src/store/reducers/ailmentReducer.js
@@ -0,0 +1,17 @@
+const LOAD_AILMENTS = "LOAD_AILMENTS";
+
+export const loadAilmentsAction = (data) => ({
+  type: LOAD_AILMENTS,
+  data,
+});
+
+export default function (state = [], action) {
+  switch (action.type) {
+    case LOAD_AILMENTS: {
+      return action.data;
+    }
+    default: {
+      return state;
+    }
+  }
+}

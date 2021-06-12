a85a7e4f535178d09082d88da8b01523ec351f34
Alycia Wang <AlyciaLWang@gmail.com>
2021-06-11 20:10:16 -0400

    Clean styling

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Workbench from "./Workbench";
+import "./Game.scss";
 
 const Game = (props) => {
   return (
-    <div>
+    <div className="GameContainer">
       <h1>Potion Matching Game</h1>
       <Workbench />
     </div>

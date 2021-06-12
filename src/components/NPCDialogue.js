f765c7b39c4f1e38176f81a5ecee4efb4356e246
Alycia Lei <alycialwang@gmail.com>
2021-06-12 18:00:51 -0400

    Add animation for npc dialogue (#22)

--- src/components/NPCDialogue.js
+++ src/components/NPCDialogue.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Typist from "react-typist";
 
-const NPCDialogue = ({body}) => {
-    return (
-        <div className="dialogue-box">
-            <p>{body}</p>
-        </div>
-    )
+const NPCDialogue = ({ body }) => {
+  const [end, setEnd] = useState(false);
+  const [speech, setSpeech] = useState("");
+
+  const endBody = () => {
+    setEnd(true);
+  };
+
+  const forceEnd = () => {
+    setEnd(true);
+  };
+
+  useEffect(() => {
+    if (body !== speech) {
+      setEnd(false);
+    }
+    setSpeech(body);
+  }, [body]);
+
+  return (
+    <div className="dialogue-box" id="dialogue" onClick={forceEnd}>
+      {body && !end ? (
+        <Typist onTypingDone={endBody}>
+          <p>{body}</p>
+        </Typist>
+      ) : (
+        <p>{speech}</p>
+      )}
+    </div>
+  );
 };
 
 export default NPCDialogue;

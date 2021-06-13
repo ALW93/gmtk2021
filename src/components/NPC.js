c8db5a0ab7beb250b05390664856fb4313870fee
Alycia Wang <AlyciaLWang@gmail.com>
2021-06-13 00:11:36 -0400

    Add facial reactions

--- src/components/NPC.js
+++ src/components/NPC.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-
+import NPCPortrait from "./NPCPortrait";
 import NPCDialogue from "./NPCDialogue";
 import images from "../images/images";
 
 const NPC = () => {
-  const {npcs, ailments} = useSelector(state => state);
+  const { npcs, ailments } = useSelector((state) => state);
   const potionId = useSelector((state) => state.active.potion);
-  const npcId = useSelector(state => state.active.npc)
-  const [npc, setNpc] = useState(npcs[npcId])
+  const npcId = useSelector((state) => state.active.npc);
+  const [npc, setNpc] = useState(npcs[npcId]);
   const [isMatchingPotion, setIsMatchingPotion] = useState(false);
   const [dialogue, setDialogue] = useState("");
+  const [reaction, setReaction] = useState("standard");
 
   useEffect(() => {
     // let ailmentWhichSelectedPotionCures;
@@ -21,28 +22,29 @@ const NPC = () => {
     // })
 
     if (potionId && potionId === ailments[npc.ailment].cure) {
-        setIsMatchingPotion(true);
+      setIsMatchingPotion(true);
     }
   }, [potionId, npc, ailments]);
 
   useEffect(() => {
     if (potionId) {
       setDialogue(isMatchingPotion ? npc?.success : npc?.failure);
+      setReaction(isMatchingPotion ? "happy" : "standard");
     } else {
       setDialogue(npc?.intro);
     }
   }, [npc, isMatchingPotion, potionId]);
 
-    useEffect(() => {
-      setNpc(npcs[npcId])
-    }, [npcId])
+  useEffect(() => {
+    setNpc(npcs[npcId]);
+  }, [npcId]);
 
-  if (!npc) return null; 
+  if (!npc) return null;
 
   return (
     <div className="Npc">
       <div className="imageContainer">
-        <img src={images[npc.id]} alt={npc.name} />
+        <NPCPortrait id={npc.id} mode={reaction} />
       </div>
       <NPCDialogue body={dialogue} name={npc.name} />
     </div>

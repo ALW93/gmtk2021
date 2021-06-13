ec2daa5606d8659d90184b5e8deffea7785c59d5
Alicia Mira Kim <alicia.mira.kim@gmail.com>
2021-06-13 14:33:14 -0400

    as dfads

--- src/components/StartMenu.js
+++ src/components/StartMenu.js
@@ -42,8 +42,10 @@ const StartMenu = ({setMusicPlaying}) => {
           </Link>
         </div>
       </div>}
-      <footer>
-        <a href='https://www.freepik.com/vectors/book'>Book vector created by upklyak - www.freepik.com</a>
+      <footer style={{position: "absolute", "bottom": 0}}>
+        <small>
+          <a href='https://www.freepik.com/vectors/book'>Background magic classroom vector created by upklyak - www.freepik.com</a>
+        </small>
       </footer>
     </div>
   );
